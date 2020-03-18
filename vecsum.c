119cd95b9e79e59e9e58961fdaa233ce0e3bbbfb
João Victor <joaoaraujo.367@gmail.com>
2020-03-18 11:34:06 -0300

    Initial commit

---
+++ vecsum.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+
+#define TAM 10
+
+typedef struct {
+  int *a;
+  int *b;
+  int *c;
+  int begin_sec;
+  int end_sec;
+  int id_sec;
+} thread_data_t;
+
+void init_random(int *vector);
+
+void *vecsum(void *arg);
+
+int main(int argc, char *argv[])
+{
+  srand((unsigned) time(NULL));
+
+  int rc, begin, end;
+  int num_threads;
+  int a[TAM], b[TAM];
+
+  num_threads = atoi(argv[1]);
+
+  init_random(a);
+  init_random(b);
+
+  pthread_t thr[num_threads];
+
+  thread_data_t thr_data[num_threads];
+
+  int *c = (int *) malloc(num_threads * sizeof(int));
+
+  int tam_sector = TAM / num_threads;
+
+  for (int i = 0; i < num_threads; ++i) {
+    begin = i * tam_sector;
+    end = begin + tam_sector;
+    thr_data[i].id_sec = i;
+    thr_data[i].begin_sec = begin;
+    thr_data[i].end_sec = end;
+    thr_data[i].a = a;
+    thr_data[i].b = b;
+    thr_data[i].c = c;
+    if ((rc = pthread_create(&thr[i], NULL, vecsum, &thr_data[i]))) {
+      fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
+      return EXIT_FAILURE;
+    }
+  }
+
+  for (int i = 0; i < num_threads; ++i) {
+    pthread_join(thr[i], NULL);
+  }
+
+  printf("A: ");
+  for (int i = 0; i < TAM; ++i) {
+    printf("%d ", a[i]);
+  }
+
+  printf("\nB: ");
+  for (int i = 0; i < TAM; ++i) {
+    printf("%d ", b[i]);
+  }
+
+  printf("\nC: ");
+  for (int i = 0; i < num_threads; ++i) {
+    printf("%d ", c[i]);
+  }
+  printf("\n");
+
+  free(c);
+
+  return 0;
+}
+
+
+void init_random(int *vector)
+{
+  for (int i = 0; i < TAM; ++i) {
+    vector[i] = rand() % 100;
+  }
+}
+
+void *vecsum(void *arg)
+{
+  thread_data_t *thr_data = (thread_data_t *) arg;
+  int id = thr_data->id_sec;
+  int begin = thr_data->begin_sec;
+  int end = thr_data->end_sec;
+
+  thr_data->c[id] = 0;
+  for (int i = begin; i < end; ++i) {
+    thr_data->c[id] += thr_data->a[i] + thr_data->b[i];
+  }
+  
+  pthread_exit(NULL);
+}
